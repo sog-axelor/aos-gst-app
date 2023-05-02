6591c255575c6c94b309fb9790fd08b24713f4fd
Sonu <s.gupta@axelor.com>
2023-05-02 19:01:12 +0530

    purchse state issue

--- src/main/java/com/axelor/apps/gst/impl/PurchaseLineOrderImpl.java
+++ src/main/java/com/axelor/apps/gst/impl/PurchaseLineOrderImpl.java
@@ -9,12 +9,23 @@ import java.util.Map;
 
 public class PurchaseLineOrderImpl extends PurchaseOrderLineServiceProjectImpl {
 
-  @Override
-  public Map<String, BigDecimal> compute(
-      PurchaseOrderLine purchaseOrderLine, PurchaseOrder purchaseOrder) throws AxelorException {
+	@Override
+	public Map<String, BigDecimal> compute(PurchaseOrderLine purchaseOrderLine, PurchaseOrder purchaseOrder)
+			throws AxelorException {
 
-    purchaseOrderLine.setIgst(new BigDecimal(100));
-
-    return super.compute(purchaseOrderLine, purchaseOrder);
-  }
+		BigDecimal exTaxTotal;
+		BigDecimal inTaxTotal;
+		
+		  if (purchaseOrder.getCompany().getAddress().getState() != purchaseOrder.getContactPartner().getMainAddress().getState()) {
+			  
+			  purchaseOrderLine.setSgst(BigDecimal.ZERO);
+			  purchaseOrderLine.setCgst(BigDecimal.ZERO);
+			  purchaseOrderLine.setIgst(
+			  purchaseOrderLine.getExTaxTotal().multiply(purchaseOrderLine.getGstRate()).divide(new BigDecimal(100)));
+			  
+			  
+		    } 
+		    
+		return super.compute(purchaseOrderLine, purchaseOrder);
+	}
 }
